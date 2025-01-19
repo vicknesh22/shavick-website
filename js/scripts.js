3bfe130998458a827665d203dee62425cef09456
vicknesh <vicknesh.r@mongodb.com>
2025-01-19 15:08:06 +0530

    map key changes

--- js/scripts.js
+++ js/scripts.js
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 10.95130, lng: 79.82596};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
@@ -255,7 +255,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: 10.951038336802268, lng: 79.82154494032505};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
